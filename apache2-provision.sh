b8f2273eeee8e68df6f35abc0897e73eabaa5c4d
drodcab268 <danielroca26@gmail.com>
2025-11-28 11:11:38 +0100

    modification of apache provision for web

--- apache2-provision.sh
+++ apache2-provision.sh
@@ -6,16 +6,22 @@
 # - atenea.olimpo.test.conf
 #
 
-# tu código aquí
+# tu código aquí:
+cp /vagrant/files/mercurio.olimpo.test/apache2.conf /etc/apache2/apache2.conf
+cp /vagrant/files/mercurio.olimpo.test/apolo.olimpo.test.conf /etc/apache2/sites-available/
+cp /vagrant/files/mercurio.olimpo.test/atenea.olimpo.test.conf /etc/apache2/sites-available/
 
 #
 # Habilitar los servidores virtuales
 #
 
-# tu código aquí
-
+# tu código aquí:
+a2ensite apolo.olimpo.test.conf
+a2ensite atenea.olimpo.test.conf
+a2dissite 000-default.conf
 #
 # Reiniciar el servicio
 #
 
 # tu código aquí
+systemctl restart apache2
